c603ad1c108c666ea5be0b527d0f0cd066d9e5c5
LemmoTresto <maxberkelmans@live.nl>
2017-07-12 15:46:30 +0200

    Add files via upload

---
+++ webcord/__init__.py
@@ -0,0 +1,17 @@
+"""
+
+
+
+
+
+"""
+
+__title__ = 'Webcord'
+__author__ = 'LemmoTresto'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 LemmoTresto'
+__version__ = '0.1'
+
+from .webhook import Webhook
+from .embed import Embed
+from .colour import Color, Colour
