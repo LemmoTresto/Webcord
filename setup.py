20f61abdf2ae00a9d258b177b50f3b5d14bf4e5c
LemmoTresto <maxberkelmans@live.nl>
2017-07-12 15:48:43 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'webcord',
+  packages = ['webcord'],
+  version = '0.1',
+  description = 'A Discord Webhook API Wrapper',
+  author = 'Max Berkelmans',
+  author_email = 'maxberkelmans@live.nl',
+  url = 'https://github.com/maximinijanijos/webcord',
+  download_url = 'https://github.com/maximinijanijos/webcord/archive/0.1.tar.gz',
+  keywords = ['wrapper', 'api', 'discord', 'webhooks'], # arbitrary keywords
+  classifiers = [],
+)
